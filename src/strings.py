ade0947d14324c872715f1a7a06690b873e41236
cmgann <casey.m.gann@gmail.com>
2021-11-07 19:04:56 -0600

    updated virtual env

--- src/strings.py
+++ src/strings.py
@@ -1,12 +1,9 @@
 import pyautogui
-import time
 import pyperclip
 import os
 from mytools import get_hotstrings, get_hot_string_text
 
 def write(combo):
-    #print('in s.write')
-    #print(combo)
     # checks if the combination matches a hotstring
     if combo in get_hotstrings():
         #print(f'The {combo} is valid')
@@ -16,7 +13,7 @@ def write(combo):
         with pyautogui.hold(['alt']):
             with pyautogui.hold(['shift']):
                 pyautogui.press(['left'])
-        pyautogui.press('delete')
+        pyautogui.press('delete')#
 
         # stores desired output to clipboard
         pyperclip.copy(text)
@@ -27,13 +24,12 @@ def write(combo):
             with pyautogui.hold('command'):
                 pyautogui.press('v')
         # all others
-        else:
-            print('did not work')
-            #with pyautogui.hold('control'):
-                #pyautogui.press('v')
+        #else:
+            with pyautogui.hold('control'):
+                pyautogui.press('v')
 
     else:
-        print('combo not valid')
+        #print('combo not valid')
         pass
 
 
